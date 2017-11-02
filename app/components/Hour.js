c44b6c534f4c7f91441d6812f73f6fc867d570ac
RuthSalvador <ruth.abby.sz@gmail.com>
2017-11-01 23:47:55 -0500

    añade estructura componente Hour

--- app/components/Hour.js
+++ app/components/Hour.js
@@ -1,1 +1,24 @@
-/* Acá se crerá el componente de la hora */
+/* Acá se creará el componente de la hora */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Hour = ({ city, hours, minutes }) => {
+  return (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-xs-6 col-md-3 col-lg-2'>
+          <h2>{city}</h2>
+          <p><span>{hours}</span><span>:</span><span>{minutes}</span></p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+Hour.propTypes = {
+  city: PropTypes.string.isRequired,
+  hours: PropTypes.number.isRequired,
+  minutes: PropTypes.number.isRequired
+};
+
+export default Hour
