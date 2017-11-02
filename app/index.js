c85cdf8d3135145ba67d5feb6b19548e0845be8a
michellesegv <michelle@laboratoria.la>
2017-11-02 00:16:07 -0500

    añadiendo componente Header

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import './styles/base.css'
-import React from 'react'
-import { render } from 'react-dom'
+
 import Header from './components/Header'
 import ObtainData from './components/ObtainData'
 import Hour from './components/Hour'
@@ -8,13 +7,10 @@ import Button from './components/Button'
 
 /* Acá se creará todo el contenido */
 const App = () => (
-  <Header
-    src="http://i65.tinypic.com/2lwrbwx.png"
-    alt="Laboratoria"
-  />
+
 )
 
 render(
-  <App/>,
+  <App />,
   document.getElementById('root')
 )
