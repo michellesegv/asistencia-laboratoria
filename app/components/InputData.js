6db15952561afb925b0ce18f9c49239281702a69
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-11-16 15:58:54 -0500

    merge

--- app/components/InputData.js
+++ app/components/InputData.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
 const InputData = ({ text, typeInput, id, placeholder }) => {
   return (
